0c94a4c63d4061428ac65fd59a23bc8be053306e
SarikaTest <sarika.singh400@gmail.com>
2020-10-14 21:06:28 +0530

    updates

---
+++ pattern8.java
@@ -0,0 +1,22 @@
+class Pattern8{
+public static void main (String args [])
+{
+int str = 5;
+for (int i=1;i<=5;i++)
+{
+ if(i<=3){
+ for(int j=1;j<=i;j++){
+ System.out.print("* ");
+ }
+ }
+else{
+for(int k=str;k>=1;k--){
+ System.out.print("* ");
+}}
+System.out.println();
+str--;
+}
+
+}
+
+}
